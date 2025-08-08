6e299ce801bcdfd271cbd30fb872f35e94c41d48
crystalin <alan@moonsonglabs.com>
2025-08-08 13:49:12 +0200

    fix: use ES module compatible __dirname in e2e tests
    - Replace __dirname with fileURLToPath(import.meta.url) for ES modules
    - This fixes the E2E test failures in CI

--- e2e/utils/auth-helper.ts
+++ e2e/utils/auth-helper.ts
@@ -1,7 +1,10 @@
 import { Page, Browser, BrowserContext } from '@playwright/test'
 import * as fs from 'fs'
 import * as path from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const AUTH_FILE = path.join(__dirname, '../../.auth/user.json')
 
 export class AuthHelper {
